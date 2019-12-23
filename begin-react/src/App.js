23827b2baa6a11dc4875b1fed8a99178ce60aa5d
angelajieun <ji918vgod@gmail.com>
2019-12-23 10:20:54 +0900

    useReducer 사용하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,16 +1,13 @@
-import React, { useRef, useReducer, useMemo, useCallback } from 'react';
+import React, { useRef, useReducer, useMemo, useCallback, createContext } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import useInputs from './useInputs';
 
-function countActiveUsers(users){
+function countActiveUsers(users) {
   return users.filter(user => user.active).length;
 }
 
 const initialState = {
-  inputs: {
-    username: '',
-    email: '',
-  },
   users: [
     {
       id: 1,
@@ -33,68 +30,72 @@ const initialState = {
   ]
 }
 
-function reducer(state, action){
-  switch(action.type) {
-    case 'CHANGE_INPUT':
-      // input 안에 있는 특정 값을 바꿔준다.
-      return { // 이하 모르겠음 (멀 어떻게 업데이트를 해줘야 되는건지 모르겠음)
-        ...state,
-        inputs : {
-          ...state.inputs,
-          [action.name] : action.value
-        }
-      }
-    case 'CREATE_USER' :
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE_USER':
       // input 을 초기값으로 설정
       return {
         inputs: initialState.inputs,
-        users : state.users.concat(action.user), // 모르겠음
+        users: state.users.concat(action.user), // 모르겠음
       }
-    case 'TOGGLE_USER' :
+    case 'TOGGLE_USER':
       return {
         ...state,
-        users : {
-
-        }
-
+        users: state.users.map(user =>
+          user.id === action.id
+            ? { ...user, active: !user.active }
+            : user
+        )
       }
-    default :
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        user: state.users.filter(user => user.id !== action.id)
+      }
+    default:
       return state;
   }
 }
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  // state에는 inputs, users가 들어 있는 것임
-  const {users} = state;
-  const { username, email } = state.inputs;
-  const nextId = useRef(4);
 
-  const onChange = useCallback(e => {
-    const { name, value} = e.target;
-    dispatch({
-      type : 'CHANGE_INPUT',
-      name,
-      value
-    });
-  },[]);
+export const UserDispatch = createContext(null);
+
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [form, onChange, reset] = useInputs({
+    username: '',
+    email: '',
+  });
+  const { username, email } = form;
+  const { users } = state;
+  const nextId = useRef(4);
 
   const onCreate = useCallback(() => {
     dispatch({
-      type : 'CREATE_USER',
-      user : {
+      type: 'CREATE_USER',
+      user: {
         id: nextId.current,
         username, // 위에서 받아온거 넣어줄거
         email // 동일
       }
     });
-    nextId.current + 1;
-  }, [username, email]); // 위에서 값이 변한것을 참조를 하기때문
+    nextId.current += 1;
+    reset();
+  }, [username, email, reset]); // 위에서 값이 변한것을 참조를 하기때문
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
-    <React.Fragment>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-      <UserList users={users}/>
-      <div>활성 사용자 수 : 0</div>
-    </React.Fragment>
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+      <div>활성 사용자 수 : {count}</div>
+    </UserDispatch.Provider>
   );
 }
 export default App;
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useContext } from 'react';
+import { UserDispatch } from './App';
 
-const User = React.memo(function User({ user, onRemove, onToggle}){
+const User = React.memo(function User({ user }){
   const { username, email, id, active} = user;
-  useEffect(() => {
-  },[user]);
+  const dispatch = useContext(UserDispatch);
+
   return (
     <div>
       <b
@@ -11,20 +12,20 @@ const User = React.memo(function User({ user, onRemove, onToggle}){
         color: active ? 'green' : 'black',
         cursor : 'point'
       }}
-        onClick={() => onToggle(id)}
+        onClick={() => dispatch({ type: "TOGGLE_USER", id})}
       >{username}</b>&nbsp;
       <span>{email}</span>
-      <button onClick={() => onRemove(id)}>삭제</button>
+      <button onClick={() => dispatch({ type: "REMOVE_USER", id})}>삭제</button>
     </div>
   )
 });
-function UserList({ users, onRemove, onToggle }) {
+function UserList({ users }) {
   return (
     <div>
       {
         users.map(
           user => (
-          <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
+          <User user={user} key={user.id} />
           )
         )
       }
